893ebdcfc6a8bd865016108a8c86d4245b043b04
anna-grinker <grinker.anna@gmail.com>
2023-12-07 18:42:42 +0200

    added method

--- src/main/java/org/example/Second.java
+++ src/main/java/org/example/Second.java
@@ -3,6 +3,9 @@ package org.example;
 public class Second {
 
     public void methodBranch(){
-        System.out.println("brunch first create");
+        System.out.println("brunch first create + method");
+
+
+
     }
 }
